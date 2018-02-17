63830a097079713a2f3a8bf649c1b9eb0935f65d
amitashukla <amita.shukla@datametica.com>
2018-02-17 23:36:47 +0530

    add rule interpreter functions

--- src/main/java/com/adqt/springservice/service/ProfilingContext.java
+++ src/main/java/com/adqt/springservice/service/ProfilingContext.java
@@ -40,14 +40,53 @@ public class ProfilingContext {
         this.rules = rules;
     }
 
-    public boolean interpretConsistencyRule(String data, String ruleValue, String rule){
-        if(rule.toUpperCase().equals("IN")){
+    public boolean interpretConsistencyRule(String data, String ruleValue, String rule) {
+        if (rule.toUpperCase().equals("IN")) {
             String[] in = ruleValue.split(",");
-            if(Arrays.binarySearch(in,data)<0)
+            if (Arrays.binarySearch(in, data) < 0)
                 return false;
             else return true;
-        }else {
-            return false;
+        }
+        return false;
+
+    }
+
+    public boolean interpretCompletenessRule(String data, String rule) {
+        if (rule.toUpperCase().equals("NOTNULL")) {
+            if (data == null || data.equals("") || data.equals("-")) {
+                return true;
+            } else return false;
+        }
+        return false;
+    }
+
+    public boolean interpretConformityRule(String data, String ruleValue, String rule) {
+        if (rule.toUpperCase().equals("NOTNULL")) {
+            List<Comparable> list = castObject(data, ruleValue);
+            if (list == null || list.size() == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean interpretAccuracyRule(Comparable data, List<Comparable> ruleValue, String rule) {
+        switch (rule.toUpperCase().trim()) {
+            case "GREATER":
+                return data.compareTo(ruleValue.get(0)) > 0;
+            case "SMALLER":
+                return data.compareTo(ruleValue.get(0)) < 0;
+            case "EQUAL":
+                return data.compareTo(ruleValue.get(0)) == 0;
+            case "BETWEEN":
+                return data.compareTo(ruleValue.get(0)) > 0 && data.compareTo(ruleValue.get(1)) < 0;
+            case "NOTEQUAL":
+                return data.compareTo(ruleValue.get(0)) != 0;
+            default:
+                return false;
         }
     }
+
 }

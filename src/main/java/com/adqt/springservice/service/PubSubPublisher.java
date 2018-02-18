1abce85dc8eaa830e3f475e2bf20c3680d17d706
amitashukla <amita.shukla@datametica.com>
2018-02-18 13:48:35 +0530

    rule function checks null

--- src/main/java/com/adqt/springservice/service/PubSubPublisher.java
+++ src/main/java/com/adqt/springservice/service/PubSubPublisher.java
@@ -1,9 +1,5 @@
 package com.adqt.springservice.service;
 
-import com.google.api.core.ApiFuture;
-import com.google.api.core.ApiFutureCallback;
-import com.google.api.core.ApiFutures;
-import com.google.api.gax.rpc.ApiException;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
@@ -12,27 +8,23 @@ import com.google.pubsub.v1.TopicName;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 
 public class PubSubPublisher {
-    public static void publishMessages() throws Exception {
+    public static void publishMessages(String dataPath) throws Exception {
 
-        TopicName topicName = TopicName.of("gcp-data-engineer-188205", "stock_tata_steel");
+        TopicName topicName = TopicName.of("gcp-data-engineer-188205", "adqt");
         Publisher publisher = null;
         try {
             // Create a publisher instance with default settings bound to the topic
             publisher = Publisher.newBuilder(topicName).build();
-            BufferedReader br = new BufferedReader(new FileReader(new File("/home/amitashukla0906/LetUsSee/let_us_see/src/main/java/templates/TataSteelDataSet.csv")));
+            BufferedReader br = new BufferedReader(new FileReader(new File(dataPath)));
 
             String message;
             for (; (message = br.readLine()) != null; ) {
                 //final String message = br.readLine();
                 ByteString data = ByteString.copyFromUtf8(message);
                 PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
-                ApiFuture<String> future = publisher.publish(pubsubMessage);
+                publisher.publish(pubsubMessage);
                 /*// add an asynchronous callback to handle success / failure
                 ApiFutures.addCallback(future, new ApiFutureCallback<String>() {
                     @Override
@@ -60,7 +52,9 @@ public class PubSubPublisher {
     }
 
     public static void main(String... args) throws Exception {
-        publishMessages();
+        // "/home/amitashukla0906/LetUsSee/src/main/resources/EcommerceData.csv"
+        String filePath = args[0];
+        publishMessages(filePath);
     }
 
 }
